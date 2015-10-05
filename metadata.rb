1c481228c6bb687f30db7078fb8e25126e471165
ctdk <jbingham@gmail.com>
2015-10-05 16:31:45 -0700

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeremy@raintank.io'
 license          'Apache 2.0'
 description      'Installs/Configures raintank-stack'
 long_description 'Installs/Configures raintank-stack'
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'docker', '~> 0.37.0'
 depends 'rabbitmq', '~> 4.0.0'
