293cf941c6ec565d5ba54b935e1c533aae24216e
ctdk <jbingham@gmail.com>
2015-11-18 14:35:05 -0800

    Update tests

--- test/integration/default/serverspec/metric_tank_spec.rb
+++ test/integration/default/serverspec/metric_tank_spec.rb
@@ -9,4 +9,7 @@ describe "metric_tank" do
   it "has metric_tank enabled" do
     expect(service("metric_tank")).to be_enabled
   end
+    it "is listening on port 18763" do
+    expect(port(18763)).to be_listening
+  end
 end
