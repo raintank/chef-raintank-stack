6a19b903bbc9f2556de507a278d77256dc20e750
woodsaj <anthony@monkey.id.au>
2015-12-21 16:05:02 +0800

    fix typo

--- recipes/graphite_raintank.rb
+++ recipes/graphite_raintank.rb
@@ -86,6 +86,7 @@ template "/etc/graphite-raintank.yaml" do
     log_level: node['raintank_stack']['graphite_tank']['log_level'],
     log_dir: node['raintank_stack']['graphite_tank']['log_dir'],
     index_name: node['raintank_stack']['nsq_tools']['metrics_to_elasticsearch']['index_name']
+  })
   notifies :restart, 'service[graphite-raintank]'
 end
 
